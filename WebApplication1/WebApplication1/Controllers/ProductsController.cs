35eeeb5348bf71dbb4aa05dd853927564257f095
ryan groesch <ryan.groesch@gmail.com>
2022-12-23 14:12:34 -0800

    First solution api for products tutorial

---
+++ WebApplication1/WebApplication1/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProductsApp.Models;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    [Microsoft.AspNetCore.Mvc.Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Top Ramen", Category = "Groceries", Price = .33M },
+            new Product { Id = 2, Name = "G I Joe", Category = "Toys", Price = 14.99M },
+            new Product { Id = 3, Name = "Screw Driver", Category = "Tools", Price = 4.99M }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null) { 
+                return (IHttpActionResult)NotFound();
+            }
+            return (IHttpActionResult)Ok(product); 
+        }
+    }
+}
---
+++ WebApplication1/WebApplication1/Pages/Product.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProductsApp.Pages
+{
+    public class ProductModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
