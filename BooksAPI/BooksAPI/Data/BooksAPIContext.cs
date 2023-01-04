6a14cff9059651fd99454318de0ea940259eb4fe
ryan groesch <ryan.groesch@gmail.com>
2023-01-03 18:08:04 -0800

    New API project for practice - Books API

---
+++ BooksAPI/BooksAPI/Data/BooksAPIContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using BooksAPI.Models;
+
+namespace BooksAPI.Data
+{
+    public class BooksAPIContext : DbContext
+    {
+        public BooksAPIContext (DbContextOptions<BooksAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<BooksAPI.Models.Book> Book { get; set; } = default!;
+    }
+}
---
+++ ProductsApp/ProductsApp/Controllers/ProductsController.cs
@@ -0,0 +1,32 @@
+﻿using ProductsApp.Models;
+using System.Web.Http;
+
+namespace ProductsApp.Controllers
+{
+    [Route("api/[controller]/{id}")]
+    public class ProductsController : ApiController
+    {
+        Product[] products = new Product[]
+        {
+            new Product { Id = 1, Name = "Top Ramen", Category = "Groceries", Price = .33M },
+            new Product { Id = 2, Name = "G I Joe", Category = "Toys", Price = 14.99M },
+            new Product { Id = 3, Name = "Screw Driver", Category = "Tools", Price = 4.99M }
+        };
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return products;
+        }
+
+        public IHttpActionResult GetProduct(int id)
+        {
+            Console.WriteLine("Got to the controller correctly");
+            var product = products.FirstOrDefault((p) => p.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product); 
+        }
+    }
+}
